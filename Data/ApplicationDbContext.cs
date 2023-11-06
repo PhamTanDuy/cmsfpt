d3695a7483074dc9ed80e0ffdddbb546e5384042
PhamTanDuy <117764544+PhamTanDuy@users.noreply.github.com>
2023-11-06 18:18:10 +0700

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FptLearningSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FptLearningSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<TrainerTopic> TrainerTopics { get; set; }
+        public DbSet<TraineeCourse> TraineeCourses { get; set; }
+    }
+}
