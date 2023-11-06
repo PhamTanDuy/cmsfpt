84fa71e16a38dde32906701fc46df18ad2a8dd51
PhamTanDuy <117764544+PhamTanDuy@users.noreply.github.com>
2023-11-06 18:50:08 +0700

    Add files via upload

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FptLearningSystem.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ Models/ViewModels/TrainerTopicViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FptLearningSystem.Models.ViewModels
+{
+    public class TrainerTopicViewModel
+    {
+        public TrainerTopic TrainerTopic { get; set; }
+        public IEnumerable<ApplicationUser> Users { get; set; }
+        public IEnumerable<Topic> Topics { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
